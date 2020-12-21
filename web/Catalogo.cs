1fa015a1ca2d7a9b07aefd2a4af5955a7a6e2f23
Ana Andrade <anacrln.andrade@gmail.com>
2020-12-21 00:23:03 -0300

    feat: new_examples

---
+++ web/Catalogo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web
+{
+    public class Catalogo : ICatalogo
+    {
+        public List<Livro> GetLivros()
+        {
+
+
+            var livros = new List<Livro>();
+            livros.Add(new Livro("001", "Quem mexeu", 21.55m));
+            livros.Add(new Livro("002", "Bom 2", 25.58m));
+            livros.Add(new Livro("003", "Quem 3", 71.99m));
+
+            return livros;
+        }
+    }
+}
